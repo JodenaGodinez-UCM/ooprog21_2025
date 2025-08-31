955eb60c9102df7009670a9320f6057fe10b2e09
JodenaGodinez-UCM <jodenagodinez@gmail.com>
2025-08-31 17:54:20 +0800

    Add files via upload

---
+++ chapter_1/Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+ public static void main(String [] args) {
+ 
+  //single line
+   System.out.println("Single line comment");
+   
+  /*Multi line*/
+   System.out.println("Multi line comment");
+   
+   /**
+   *Documentation 
+   */
+    System.out.println("Documentation comment");
+    
+    }
+   }
---
+++ chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Boba Tea (Gnarly), Tesla (Gnarly), Fried Chicken (Gnarly), Party in the hollywood hills");
+        System.out.println("Making beats, for a boring dumb chick");
+        System.out.println("Fricking Gnarly");
+        System.out.println("Je-Je-Jealous of my Mansion?");
+    }
+}
