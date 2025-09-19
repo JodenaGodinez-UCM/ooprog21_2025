afdbbb81ef3df4ca3de6c93f3eeff7e382147d09
JodenaGodinez-UCM <jodenagodinez@gmail.com>
2025-09-19 08:35:11 +0800

    Add files via upload

--- Chapter_3/CompanyInfo.java
+++ Chapter_3/CompanyInfo.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
   class CompanyInfo {
    public static void main(String[] args) {
--- Chapter_3/DemoGrossPay.java
+++ Chapter_3/DemoGrossPay.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
  class DemoGrossPay {
   public static void main(String[] args){
